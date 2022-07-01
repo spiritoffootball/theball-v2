3f1c21d9459b773d40b31cad35d76a32cce8288d
Christian Wach <needle@haystack.co.uk>
2022-07-01 14:01:19 +0100

    Set version to 1.0.1

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
  *
  * Bumping this will cause styles and scripts to be refreshed.
  */
-define( 'THE_BALL_V2_THEME_VERSION', '1.5.2' );
+define( 'THE_BALL_V2_THEME_VERSION', '1.0.1' );
 
 
 
