b39870a501ee8a911af83c73c0efa3eaee2d165a
Christian Wach <needle@haystack.co.uk>
2022-07-03 14:12:40 +0100

    TLC for the custom javascript

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,53 +18,25 @@ document.write( '<style type="text/css" media="screen">#the-ball-v2-search{displ
 jQuery(document).ready( function($) {
 
 	/**
-	 * Add a class to the News Item when mouse is over it.
-	 *
-	 * @since 1.0
-	 */
-	$('#page').on( 'mouseover', '#news .post, #blog .post', function( event ) {
-		$(this).addClass('newsblock-in');
-	});
-
-	/**
-	 * Remove class from the textblock when mouse moves out of it.
-	 *
-	 * @since 1.0
-	 */
-	$('#page').on( 'mouseout', '#news .post, #blog .post', function( event ) {
-		$(this).removeClass('newsblock-in');
-	});
-
-	/**
-	 * Act on clicks by going to permalink.
-	 *
-	 * @since 1.0
-	 */
-	$('#page').on( 'click', '#news .post, #blog .post', function( event ) {
-		var url = $('.entry-title a', this).prop( 'href' );
-		document.location.href = url;
-	});
-
-	/**
 	 * Act on page menu clicks by scrolling to permalink.
 	 *
-	 * @since 1.4.4
+	 * @since 1.0.0
 	 */
 	$('#page').on( 'click', '.the-ball-v2-page-menu a', function( event ) {
 
 		var scroll_offset = 0;
 
-		// disable native behaviour
+		// Disable native behaviour.
 		if ( event.preventDefault ) {
 			event.preventDefault();
 		}
 
-		// get offset
+		// Get offset.
 		if ( $('#wpadminbar').length > 0 ) {
 			scroll_offset = $('#wpadminbar').height();
 		}
 
-		// scroll to target
+		// Scroll to target.
 		$(window).stop(true).scrollTo(
 			$(this).attr( 'href' ),
 			{ duration: 1200, axis: 'y', offset: 0 - scroll_offset }
@@ -75,7 +47,7 @@ jQuery(document).ready( function($) {
 	/**
 	 * Act on clicks on the search link.
 	 *
-	 * @since 1.4.8
+	 * @since 1.0.0
 	 */
 	$('#page').on( 'click', '.the-ball-v2-search-trigger', function( event ) {
 		event.preventDefault();
@@ -85,7 +57,7 @@ jQuery(document).ready( function($) {
 	/**
 	 * Hide search form when mask is clicked.
 	 *
-	 * @since 1.4.8
+	 * @since 1.0.0
 	 */
 	$('#page').on( 'click', '#the-ball-v2-search', function( event ) {
 		$(this).hide();
@@ -95,7 +67,7 @@ jQuery(document).ready( function($) {
 	/**
 	 * Prevent search form clicks from propagating.
 	 *
-	 * @since 1.4.8
+	 * @since 1.0.0
 	 */
 	$('#page').on( 'click', '#the-ball-v2-search form', function( event ) {
 		event.stopPropagation();
