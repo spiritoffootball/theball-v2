b5b73baff36317cad14b93ade99ffc80cf9ef8cd
Christian Wach <needle@haystack.co.uk>
2022-07-22 00:40:54 +0100

    Bump version to 1.1.4

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
  *
  * Bumping this will cause styles and scripts to be refreshed.
  */
-define( 'THE_BALL_V2_THEME_VERSION', '1.1.3' );
+define( 'THE_BALL_V2_THEME_VERSION', '1.1.4' );
 
 // Set theme debugging state.
 if ( ! defined( 'THE_BALL_V2_THEME_DEBUG' ) ) {
