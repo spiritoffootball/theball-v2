cdef04c698829e07990a2b6b764f5cdc41d189f2
Christian Wach <needle@haystack.co.uk>
2026-04-15 21:40:36 +0100

    Prevent fatal error when Event Organiser is not present

--- functions.php
+++ functions.php
@@ -479,6 +479,11 @@ function the_ball_v2_events_sort( $orderby, $query ) {
 		return $orderby;
 	}
 
+	// Bail if EO is not present.
+	if ( ! function_exists( 'eventorganiser_is_event_query' ) ) {
+		return $orderby;
+	}
+
 	// Bail if not exclusively an EO orderby query.
 	if ( ! eventorganiser_is_event_query( $query, true ) ) {
 		return $orderby;
